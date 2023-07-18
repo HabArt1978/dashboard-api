0052320bb4d4e8fe251120edd92518efdb507564
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-07-18 15:09:03 +0300

    - простой сервер

---
+++ index.js
@@ -0,0 +1,23 @@
+import http from "http"
+
+const host = "127.0.0.1"
+const port = 8000
+
+const server = http.createServer((request, response) => {
+  switch (request.method) {
+    case "GET":
+      switch (request.url) {
+        case "/hello":
+          response.statusCode = 200
+          response.setHeader("Content-Type", "text/plain")
+          response.end("Привет!")
+          break
+      }
+
+      break
+  }
+})
+
+server.listen(port, host, () => {
+  console.log(`Сервер запущен на ${host}:${port}`)
+})
