fb288fe6340d064fd6370679e9c9fc70ee447ddb
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-07-20 01:29:30 +0300

    - установил TS для приложения - установил @types/express

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response, NextFunction } from "express"
 import userRouter from "./users/users.js"
 
 const port = 8000
@@ -15,10 +15,12 @@ app.get("/hello", (request, response) => {
 
 app.use("/users", userRouter)
 
-app.use((error, request, response, next) => {
-  console.log(error.message)
-  response.status(500).send(error.message)
-})
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    console.log(error.message)
+    response.status(500).send(error.message)
+  },
+)
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`)
