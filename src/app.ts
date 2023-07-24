64a19c5501a15e62b9b5a599803a0c81d2e4806a
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-07-25 00:17:07 +0300

    - добавил обработку ошибок

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import express, { Express } from "express"
 import { Server } from "node:http"
 import { LoggerService } from "./logger/logger.service"
 import { UsersController } from "./users/users.controller"
+import { ExeptionFilter } from "./errors/exeption.filter"
 
 export class App {
   app: Express
@@ -12,20 +13,32 @@ export class App {
   port: number
   logger: LoggerService
   userController: UsersController
+  exeptionFilter: ExeptionFilter
 
-  constructor(loger: LoggerService, userController: UsersController) {
+  constructor(
+    loger: LoggerService,
+    userController: UsersController,
+    exeptionFilter: ExeptionFilter,
+  ) {
     this.app = express()
     this.port = 8000
     this.logger = loger
     this.userController = userController
+    this.exeptionFilter = exeptionFilter
   }
 
   useRoutes() {
     this.app.use("/users", this.userController.router)
   }
+  //  метод отлавливает ошибки с определённым statusCode и отдаёт statusCode ошибки потребителю.
+  useExeptionFilters() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
+  }
+
   // метод инициализации приложения
   public async init() {
     this.useRoutes()
+    this.useExeptionFilters()
     this.server = this.app.listen(this.port)
     this.logger.log(`Сервер запущен на http://localhost:${this.port}`)
   }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 // модуль точки входа приложения
 
 import { App } from "./app"
+import { ExeptionFilter } from "./errors/exeption.filter"
 import { LoggerService } from "./logger/logger.service"
 import { UsersController } from "./users/users.controller"
 
 const bootstrap = async () => {
   const logger = new LoggerService()
-  const app = new App(logger, new UsersController(logger))
+  const app = new App(
+    logger,
+    new UsersController(logger),
+    new ExeptionFilter(logger),
+  )
   await app.init()
 }
 
