4e7ceadd763ff16d7a7af8a6f12d012d35a987e0
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-07-30 18:16:31 +0300

    - добавил Eslint и Prettier, настроил конфигурацию

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,9 +1,11 @@
 // типизация роута контроллера
 
-import { NextFunction, Request, Response, Router } from "express"
+import { NextFunction, Request, Response, Router } from 'express';
 
 export interface IControllerRoute {
-  path: string
-  func: (req: Request, res: Response, next: NextFunction) => void
-  method: keyof Pick<Router, "get" | "post" | "delete" | "patch" | "put">
+	path: string;
+	func: (req: Request, res: Response, next: NextFunction) => void;
+	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
 }
+
+export type ExpressReturnType = Response<any, Record<string, any>>;
