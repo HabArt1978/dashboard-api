2f5451f81155fcd3965721f3918903867d3b7328
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-08-01 02:10:20 +0300

    - установил  middleware body-parser для node.js - добавил объекты передачи данных ( dto ) для login и regicter запросов

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,8 +4,10 @@ import { HTTPError } from '../errors/http-error.class';
 import { injectable, inject } from 'inversify';
 import { TYPES } from '../types';
 import { ILogger } from '../logger/logger.interface';
-import 'reflect-metadata';
 import { IUserController } from './user.controller.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import 'reflect-metadata';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -17,11 +19,13 @@ export class UserController extends BaseController implements IUserController {
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
+	login({ body }: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(body);
 		next(new HTTPError(401, 'ошибка авторизации', 'login'));
 	}
 
-	register(req: Request, res: Response, next: NextFunction): void {
+	register({ body }: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
+		console.log(body);
 		this.ok(res, 'register');
 	}
 }
