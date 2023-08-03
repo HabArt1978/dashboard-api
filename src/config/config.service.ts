dff4f692f64fa280aa527bb5af8e4670b64c403f
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-08-04 00:38:32 +0300

    - установил библиотеку ( dotenv ) - реализовал сервис конфигурации ( возможность работы с .env )

---
+++ src/config/config.service.ts
@@ -0,0 +1,24 @@
+import { inject, injectable } from 'inversify';
+import { IConfigService } from './config.service.interface';
+import { config, DotenvConfigOutput, DotenvParseOutput } from 'dotenv';
+import { TYPES } from '../types';
+import { ILogger } from '../logger/logger.interface';
+
+@injectable()
+export class ConfigService implements IConfigService {
+	private config: DotenvParseOutput;
+
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
+		const results: DotenvConfigOutput = config();
+		if (results.error) {
+			this.logger.error('[ConfigService] Не удалось прочитать файл .env или он отсутствует');
+		} else {
+			this.logger.log('[ConfigService] Конфигурация .env загружена');
+			this.config = results.parsed as DotenvParseOutput;
+		}
+	}
+
+	get(key: string): string {
+		return this.config[key];
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import { IExeptionFilter } from './errors/exeption.filter.interface';
 import { IUserService } from './users/users.service.interface';
 import { UserService } from './users/users.service';
 import { IUserController } from './users/user.controller.interface';
+import { IConfigService } from './config/config.service.interface';
+import { ConfigService } from './config/config.service';
 
 export interface IBootstrapReturn {
 	appContainer: Container;
@@ -19,10 +21,11 @@ export interface IBootstrapReturn {
 
 // Данный контейнер будет отделяемым модулем
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
-	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
 	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
 	bind<IUserController>(TYPES.UserController).to(UserController);
 	bind<IUserService>(TYPES.UserService).to(UserService);
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
