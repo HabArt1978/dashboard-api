247f413fef5e97d967174b6da58d57d76b4a0ef6
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-08-10 06:46:44 +0300

    - установил jsonwebtoken ( JWT ) - добавил подпись токена и его выдачу пользователю при логировании

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,18 @@ import { ILogger } from '../logger/logger.interface';
 import { IUserController } from './user.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { UserService } from './users.service';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
+import { IUserService } from './users.service.interface';
 import 'reflect-metadata';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private confogService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -43,7 +46,8 @@ export class UserController extends BaseController implements IUserController {
 		if (!result) {
 			return next(new HTTPError(401, 'ошибка авторизации', 'login'));
 		}
-		this.ok(res, {});
+		const jwt = await this.singJWT(body.email, this.confogService.get('SECRET'));
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -56,5 +60,21 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HTTPError(422, 'Такой пользователь уже существует!'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private singJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{ email, iat: Math.floor(Date.now() / 1000) },
+				secret,
+				{ algorithm: 'HS256' },
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
