3bfcfff3c74d8011cb5cb971e645a08ddd2ae35b
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-08-15 19:04:50 +0300

    - дополнил e2e тесты - добавил возможность анализа покрытия приложения e2e тестами ( --coverage )

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -17,6 +17,39 @@ describe('Users e2e', () => {
 		});
 		expect(res.statusCode).toBe(422);
 	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'hab_mark@hab.ru',
+			password: '12345678',
+		});
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'hab_mark@hab.ru',
+			password: '1',
+		});
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app).post('/users/login').send({
+			email: 'hab_mark@hab.ru',
+			password: '12345678',
+		});
+
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('hab_mark@hab.ru');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app).get('/users/info').set('Authorization', 'Bearer 1');
+		expect(res.statusCode).toBe(401);
+	});
 });
 
 afterAll(() => {
